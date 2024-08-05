acb4e3c5b0046cf13d1f1bade2d097f36f474dca
MadhavendraSinghShaktawat <madhavendrasinghshaktawat@gmail.com>
2024-08-05 17:08:56 +0530

    ejs-partials

---
+++ 7.EJS-partials/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const PORT = 8082;
+
+//* Serve the static file/folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//* Setting EJS sigine
+app.set("view engine", "ejs");
+
+
+
+//? Render Home page/route
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+
+//? Render About page/route
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
+
+//? Render Gallery page/route
+app.get('/gallery', (req, res)=>{
+    res.render('gallery');
+});
+
+//? Render Contact page/route
+app.get('/contact', (req, res)=>{
+    res.render('contact');
+});
+
+
+app.listen(PORT, () => console.log(`Server is working on http://localhost:${PORT}`))
